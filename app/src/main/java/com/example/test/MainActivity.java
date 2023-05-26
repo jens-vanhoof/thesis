d3f8ea4f99186e053b45ca690c0ce061db61508b
Jens <jens.vanhoof@student.kuleuven.be>
2023-05-26 16:12:07 +0200

    working

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         getSupportFragmentManager().addFragmentOnAttachListener(this);
 
         if (savedInstanceState == null) {
@@ -89,10 +90,27 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
         Switch toggleMethod = findViewById(R.id.toggleMethod);
         toggleMethod.setOnClickListener(view -> {
             LinearLayout hide = findViewById(R.id.hideSurfaceArea);
+            List<Node> children = new ArrayList<>(arFragment.getArSceneView().getScene().getChildren());
+            for (Node node : children) {
+                if (node instanceof AnchorNode) {
+                    if (((AnchorNode) node).getAnchor() != null) {
+                        ((AnchorNode) node).getAnchor().detach();
+                        arFragment.getArSceneView().getScene().removeChild(node);
+                    }
+                }
+
+            }
             if (toggleMethod.isChecked()){
                 hide.setVisibility(View.VISIBLE);
-            }
-            else{
+                if(source=="cylinder"||source=="pyramid"){
+                    LinearLayout info = findViewById(R.id.info);
+                    info.setOrientation(LinearLayout.VERTICAL);
+                }
+            }else{
+                if(source=="cylinder"||source=="pyramid"){
+                    LinearLayout info = findViewById(R.id.info);
+                    info.setOrientation(LinearLayout.HORIZONTAL);
+                }
                 hide.setVisibility(View.INVISIBLE);
             }
         });
@@ -123,6 +141,18 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
             dimensions.add(x);
             dimensions.add(x);
             dimensions.add(z);
+            TextView formula = findViewById(R.id.formula);
+            Switch toggleMethod = findViewById(R.id.toggleMethod);
+            Vector3 refactor = new Vector3(x,x,z);
+            if(toggleMethod.isChecked()){
+                LinearLayout info = findViewById(R.id.info);
+                info.setOrientation(LinearLayout.VERTICAL);
+                formula.setText(String.format("A=2\u03C0rh+2\u03C0r\u00B2=2\u03C0%.2f%.2f+2\u03C0%.2f\u00B2=",refactor.x,refactor.z,refactor.x));
+            }else{
+                LinearLayout info = findViewById(R.id.info);
+                info.setOrientation(LinearLayout.HORIZONTAL);
+                formula.setText(String.format("V=\u03C0r\u00B2h=\u03C0%.2f\u00B2%.2f=",refactor.x,refactor.z));
+            }
 
             loadModel(model);
             diameter = true;
@@ -154,6 +184,14 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
             dimensions.add(x);
             dimensions.add(y);
             dimensions.add(z);
+            TextView formula = findViewById(R.id.formula);
+            Switch toggleMethod = findViewById(R.id.toggleMethod);
+            Vector3 refactor = new Vector3(x,x,x);
+            if(toggleMethod.isChecked()){
+                formula.setText(String.format("A=lw+l\u221A[(w/2)\u00B2+h\u00B2]+w\u221A[(l/2)\u00B2+h\u00B2]=%1$.2f%2$.2f+l\u221A[(%2$.2f/2)\u00B2+%3$.2f\u00B2]+%2$.2f\u221A[(%1$.2f/2)\u00B2+%3$.2f\u00B2]=",refactor.x,refactor.y,refactor.z));
+            }else{
+                formula.setText(String.format("V=lwh/3=%.2f%.2f%.2f/3=",refactor.x,refactor.y,refactor.z));
+            }
             loadModel(model);
             height = true;
             width = true;
@@ -163,6 +201,10 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
         dialog.show();
     }
     public void cubeDialog(String model) {
+        if(source=="cylinder" || source=="pyramid"){
+            LinearLayout info = findViewById(R.id.info);
+            info.setOrientation(LinearLayout.HORIZONTAL);
+        }
         LayoutInflater inflater = getLayoutInflater();
         View alertLayout = inflater.inflate(R.layout.layout_custom_dialog_cube, null);
         final TextInputEditText ribbeField = alertLayout.findViewById(R.id.ribbe);
@@ -181,6 +223,15 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
             dimensions.add(x);
             dimensions.add(x);
             dimensions.add(x);
+            TextView formula = findViewById(R.id.formula);
+            Switch toggleMethod = findViewById(R.id.toggleMethod);
+            Vector3 refactor = new Vector3(x,x,x);
+            if(toggleMethod.isChecked()){
+                formula.setText(String.format("A=6a\u00B2=6%.2f\u00B2=",refactor.x));
+            }else{
+                formula.setText(String.format("V=a\u00B3=%.2f\u00B3=",refactor.x));
+            }
+
             loadModel(model);
             diameter = true;
         });
@@ -218,7 +269,6 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
                     MainActivity activity = weakActivity.get();
                     if (activity != null) {
                         Node modelNode = new Node();
-                        modelNode.setName("kaka");
                         modelNode.setRenderable(model);
                         source = name;
                         activity.model = modelNode.getRenderable();
@@ -252,6 +302,7 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
                     updateModelAnimators();
                 }
                 Switch toggleFold = findViewById(R.id.toggleFold);
+                Switch toggleMethod = findViewById(R.id.toggleMethod);
                 if(animationStates.get(currentId) == "Open"){
                     toggleFold.setChecked(true);
                 }
@@ -263,10 +314,45 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
                 updateTextFields(this.height, node, "height", vector.y);
                 updateTextFields(this.length, node, "length", vector.z);
                 updateTextFields(this.width, node, "width", vector.x);
-                double volumeValue = getVolumeValue(node.getName(),vector);
+                TextView formula = findViewById(R.id.formula);
+                if(node.getName()=="cube"){
+                    if(toggleMethod.isChecked()){
+                        formula.setText(String.format("A=6a\u00B2=6*%.2f\u00B2=",vector.x));
+                    }else{
+                        formula.setText(String.format("V=a\u00B3=%.2f\u00B3=",vector.x));
+                    }
+                }
+                if(node.getName() == "cylinder"){
+                    if(toggleMethod.isChecked()){
+                        formula.setText(String.format("A=2\u03C0rh+2\u03C0r\u00B2=2*\u03C0*%1$.2f*%2$.2f+2*\u03C0*%1$.2f\u00B2=",vector.x/2,vector.y));
+                    }else{
+                        formula.setText(String.format("V=\u03C0r\u00B2h=\u03C0*%1$.2f\u00B2*%2$.2f=",vector.x/2,vector.y));
+                    }
+                }
+                if(node.getName() == "pyramid"){
+                    if(toggleMethod.isChecked()){
+                        formula.setText(String.format("A=lw+l\u221A[(w/2)\u00B2+h\u00B2]+w\u221A[(l/2)\u00B2+h\u00B2]=%1$.2f*%2$.2f+l*\u221A[(%2$.2f/2)\u00B2+%3$.2f\u00B2]+%2$.2f*\u221A[(%1$.2f/2)\u00B2+%3$.2f\u00B2]=",vector.z,vector.x,vector.y));
+                    }else{
+                        formula.setText(String.format("V=lwh/3=%.2f*%.2f*%.2f/3=",vector.z,vector.x,vector.y));
+                    }
+                }
+                double measure = 0;
+                String method;
+                if(toggleMethod.isChecked()){
+                    method = "Surface Area";
+                }else{
+                    method = "Volume";
+                }
+                if(arFragment.getArSceneView().getScene().getChildren().size() != 1){
+                    if(toggleMethod.isChecked()){
+                        measure = getSurfaceAreaValue(node.getName(),vector);
+                    }else{
+                        measure = getVolumeValue(node.getName(),vector);
+                    }
+                }
 
-                TextView v = findViewById(R.id.volume);
-                v.setText(String.format("Volume=%.2fcm\u00B3", volumeValue));
+                TextView v = findViewById(R.id.measure);
+                v.setText(String.format("%s=%.2fcm\u00B3",method, measure));
             }
     }
 
@@ -311,10 +397,11 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
         RenderableInstance renderableInstance = model.getChildren().get(0).getRenderableInstance();
         ModelAnimator.ofAnimationFrame( renderableInstance, "Closed",this.model.getAnimationFrameRate()).start();
         animationStates.put(currentId,"Closed");
+
         createInitialModelAnimators(renderableInstance);
 
-
         Switch toggle = findViewById(R.id.toggleFold);
+        Switch toggleMethod = findViewById(R.id.toggleMethod);
         toggle.setOnClickListener(view -> {
             if (toggle.isChecked()){
                 toggle.setEnabled(false);
@@ -346,10 +433,18 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
         Vector3 widthPosition = new Vector3(0.0f, -0.08f, (float)dimensions.get(0)/100);
         Node width = addNode(model,widthPosition,"width",dimensions.get(1));
 
-        double volumeValue = getVolumeValue(model.getName(),new Vector3((float)dimensions.get(0),(float)dimensions.get(2),(float)dimensions.get(1)));
+        double measure;
+        String method;
+        if(toggleMethod.isChecked()){
+            measure = getSurfaceAreaValue(source,new Vector3((float)dimensions.get(0),(float)dimensions.get(2),(float)dimensions.get(1)));
+            method = "Surface Area";
+        }else{
+            measure = getVolumeValue(source,new Vector3((float)dimensions.get(0),(float)dimensions.get(2),(float)dimensions.get(1)));
+            method = "Volume";
+        }
 
-        TextView v = findViewById(R.id.volume);
-        v.setText(String.format("Volume=%.2fcm\u00B3", volumeValue));
+        TextView v = findViewById(R.id.measure);
+        v.setText(String.format("%s=%.2fcm\u00B3",method, measure));
         arFragment.getArSceneView().getScene().addOnUpdateListener(view -> updateDimensions());
 
 
@@ -368,6 +463,25 @@ public class MainActivity extends AppCompatActivity implements FragmentOnAttachL
         }
 
     }
+
+    private double getSurfaceAreaValue(String model, Vector3 vector) {
+        if(model == "cylinder"){
+            double r = vector.x/2;
+            double h = vector.y;
+            return 2*Math.PI * r * h + 2 * Math.PI * Math.pow(r,2);
+        }else if(model == "cube"){
+            double r = vector.x;
+            return 6*Math.pow(r,2);
+        }
+        if(model == "pyramid"){
+            double l = vector.x;
+            double w = vector.y;
+            double h = vector.z;
+            return l*w + l * Math.sqrt((Math.pow(w/2,2)+Math.pow(h,2))) + w *Math.sqrt(Math.pow(l/2,2)+Math.pow(h,2));
+        }
+        return 0;
+    }
+
     private void updateModelAnimators(){
         unfold = animators.get(currentId.toString()+",unfold");
         fold = animators.get(currentId.toString()+",fold");
